6bbd349c2466e960776efb73c85f75bb07e989c5
Manas Mengle <manmenonsense@gmail.com>
2024-12-01 17:03:42 +0000

    initial boilerplate

---
+++ README.md
@@ -0,0 +1,10 @@
+# Advent of Code 2024
+
+My solutions to [Advent Of Code](https://adventofcode.com/2024) using Python.
+
+How to run:
+
+```console
+cd ./day_1
+hatch run run
+```
---
+++ template/src/template/problem_2.py
@@ -0,0 +1,11 @@
+from result import Result, Ok, Err
+from .utils import read_input_file_as_string
+
+def main():
+    input = read_input_file_as_string().unwrap().splitlines()
+
+    result = solve_problem_2(input)
+    print(f"{result.unwrap()}")
+
+def solve_problem_2(input: str) -> Result[any, str]:
+    return Ok(0)
