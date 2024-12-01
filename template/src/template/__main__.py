3ba681d679f14fb81c773a5ceb568c1c6c3f3c72
Manas Mengle <manmenonsense@gmail.com>
2024-12-01 18:58:42 +0000

    edit template

--- template/src/template/__main__.py
+++ template/src/template/__main__.py
@@ -1,7 +1,7 @@
 import sys
 from template import problem_1, problem_2
 
-is_testing = sys.argv[1] != "tests" or sys.argv[2] != "vscode_pytest"
+is_testing = (len(sys.argv) > 1) and (sys.argv[1] != "tests" or sys.argv[2] != "vscode_pytest")
 if not is_testing:
     print("Usage: python3 -m template <input_file_name>")
     sys.exit(1)
